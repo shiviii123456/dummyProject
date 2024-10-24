392b08b5b7cfe60183235f1d480c4547ee4e0c88
shiviii123456 <rockstarswati102@gmail.com>
2024-10-25 00:21:40 +0530

    Added Day Enum

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import enumModule.Currency;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Currency curr = Currency.INR;
         System.out.println("New Git Repo created");
     }
 }
---
+++ src/enumModule/Day.java
@@ -0,0 +1,7 @@
+package enumModule;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY
+}
